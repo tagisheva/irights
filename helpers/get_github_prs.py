10213f50147ad1faaca6588690a9efaa7c537bc2
tagisheva <tanya.agisheva@eu.equinix.com>
2022-12-07 16:24:12 +0100

    SYNC and reverted prs excluded from the report

--- helpers/get_github_prs.py
+++ helpers/get_github_prs.py
@@ -1,6 +1,14 @@
 import requests
+from re import search
 
 from helpers.constants import GITHUB_USER, API_URL, headers, start_datetime, end_datetime
+
+EXCLUDE_SYNC = "SYNC"
+EXCLUDE_REVERT = "Revert"
+
+def excludeIrrelevantTitles(idx_and_item):
+    index, item = idx_and_item
+    return not search(EXCLUDE_SYNC, item["title"]) and not search(EXCLUDE_REVERT, item["title"])
 
 start_filter = start_datetime.strftime("%Y-%m-%d")
 end_filter = end_datetime.strftime("%Y-%m-%d")
@@ -9,4 +17,4 @@ queryParams = "q=author:" + GITHUB_USER + "+type:pr+org:equinix-product+review:a
 
 pull_requests_search = requests.get(API_URL + "/search/issues", params=queryParams, headers=headers)
 
-pull_requests = enumerate(pull_requests_search.json()["items"])
+pull_requests = filter(excludeIrrelevantTitles, enumerate(pull_requests_search.json()["items"]))
