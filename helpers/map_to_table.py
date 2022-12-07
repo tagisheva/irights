dbe18fd83d0b25d23cd6d5f030f403f365447f77
tagisheva <tanya.agisheva@eu.equinix.com>
2022-12-07 13:32:26 +0100

    code refactoring

--- helpers/map_to_table.py
+++ helpers/map_to_table.py
@@ -4,49 +4,63 @@ import re
 from re import search
 from docxtpl import DocxTemplate, RichText
 
-from helpers.constants import headers, JIRA
+from helpers.constants import headers, JIRA, header_column_1, header_column_2, header_column_3, header_column_4
 
 tpl=DocxTemplate("./files/template.docx")
+
+def addTableHeader():
+    return {"n": header_column_1, "cols": [RichText(header_column_2), RichText(header_column_3), RichText(header_column_4)]}
+
+def getJiraTaskDataByBranchName(branch_name):
+    hasStandartName = search("(-|\/)", branch_name)
+    split_branch_name = re.split("(-|\/)", branch_name)
+
+    return {
+        "jira_project_key": split_branch_name[2] if hasStandartName else "",
+        "jira_task_number": split_branch_name[4] if hasStandartName else ""
+    }
 
 def mapPullsIntoTableRows(idx_and_item):
     index, item = idx_and_item
     pull_url = item["pull_request"]["url"]
     pull_request = requests.get(pull_url, headers=headers)
-    pull_ref = pull_request.json()["head"]["ref"]
-    isNotSyncPR = search("(-|\/)", pull_ref)
-    split_branch_name = re.split("(-|\/)", pull_ref)
-    project_key = split_branch_name[2] if isNotSyncPR else ""
-    task_number = split_branch_name[4] if isNotSyncPR else ""
-    task_url = ""
-    if task_number.isnumeric():
-        task_url = JIRA + project_key + "-" + task_number
 
-    task_rt = RichText('')
+    branch_name = pull_request.json()["head"]["ref"]
+    jira_data = getJiraTaskDataByBranchName(branch_name)
 
-    if task_url!="":
-        task_rt.add(task_url,url_id=tpl.build_url_id(task_url),color='#0000EE')
-        task_rt.add("\a\n" + item["title"])
+    jira_project_key = jira_data["jira_project_key"]
+    jira_task_number = jira_data["jira_task_number"]
+
+    jira_task_url = ""
+    if jira_task_number.isnumeric():
+        jira_task_url = JIRA + jira_project_key + "-" + jira_task_number
+
+    work_description_cell = RichText('')
+
+    if jira_task_url!="":
+        work_description_cell.add(jira_task_url,url_id=tpl.build_url_id(jira_task_url),color='#0000EE')
+        work_description_cell.add("\a\n" + item["title"])
     else:
-        task_rt.add(item["title"])
+        work_description_cell.add(item["title"])
 
-    help_rt = RichText('      ---')
-    github_rt = RichText('')
-    github_rt.add(item["html_url"],url_id=tpl.build_url_id(item["html_url"]),color='#0000EE')
+    co_author_cell = RichText('      ---')
+    work_location_cell = RichText('')
+    work_location_cell.add(item["html_url"],url_id=tpl.build_url_id(item["html_url"]),color='#0000EE')
 
-    return { "n": str(index + 1) + ".", "cols": [task_rt, help_rt, github_rt]}
+    return { "n": str(index + 1) + ".", "cols": [work_description_cell, co_author_cell, work_location_cell]}
 
 
 def mapIssuesIntoTableRows(idx_and_item):
     index, item = idx_and_item
     issue_url = item["html_url"]
 
-    task_rt = RichText('')
-    task_rt.add("\a\n" + item["title"])
-    help_rt = RichText('      ---')
-    github_rt = RichText('')
-    github_rt.add(issue_url,url_id=tpl.build_url_id(issue_url),color='#0000EE')
+    work_description_cell = RichText('')
+    work_description_cell.add("\a\n" + item["title"])
+    co_author_cell = RichText('      ---')
+    work_location_cell = RichText('')
+    work_location_cell.add(issue_url,url_id=tpl.build_url_id(issue_url),color='#0000EE')
 
-    return { "n": str(index + 1) + ".", "cols": [task_rt, help_rt, github_rt]}
+    return { "n": str(index + 1) + ".", "cols": [work_description_cell, co_author_cell, work_location_cell]}
 
 def mapToTableData(idx_and_item):
     index, item = idx_and_item
